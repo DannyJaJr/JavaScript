d52653a5772a51572ac6dc278149bb81e3d570ee
DanielJaJr <ldanieljajr@gmail.com>
2021-04-15 11:53:33 -0400

    array

---
+++ arrays.js
@@ -0,0 +1,16 @@
+const favoriteArtist = ["SeaPaul", "Mozart", "Amadeus", "Wolfgang"];
+
+const randomEmojis = ["🤓", "🤓", "🥸"]
+
+const elitCoders = [["Janneth", "Sebastain"], ["Danny", "Ariel"], ["Dave", "Jesse"], ["Cody", "Tyler"]];
+
+console.log(randomEmojis);
+console.log(favoriteArtist );
+console.log(elitCoders);
+
+for (let i =0; i < elitCoders.length; i ++) {
+    let fullStack = elitCoders[i];
+    console.log(fullStack + " are software Developers");
+
+}
+
